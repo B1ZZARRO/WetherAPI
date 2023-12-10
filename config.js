0ac06423c84f2e07d0598d9a240aabf8fcf270fb
B1ZZARRO <isip_d.a.jeleznyak@mpt.ru>
2023-12-10 16:29:13 +0300

    1 com

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric",
+        SECRET_KEY: "14556cdb2cb15ad571db5b657b5dfa7f"
+    }
+}
+
+module.exports = constants
